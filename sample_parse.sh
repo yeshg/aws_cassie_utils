20c52b11db5c10143f00270b728166c45bdaff42
Rohan Pankaj <39844269+RohanPankaj@users.noreply.github.com>
2019-08-30 11:37:33 -0700

    now trying to replace the tags instead of deleting them

--- sample_parse.sh
+++ sample_parse.sh
@@ -11,10 +11,14 @@ read upper
 sudo cp /home/rohan/Downloads/out.txt /home/rohan/Downloads/parse_out.txt
 
 #echo `cat /home/drl/rohanp/out.txt`
-for i in {-n lower..-n upper}
-do
-cat /home/rohan/Downloads/parse_out.txt | grep exec_host
+#for i in {-n lower..-n upper}
+#do
+#cat /home/rohan/Downloads/parse_out.txt | grep exec_host
 echo= sudo cat /home/rohan/Downloads/parse_out.txt | awk -v FS="(<exec_host>|</exec_host>)" '{print $2}'
-sudo sed -i "/<exec_host>/,/exec_host>:/d" /home/rohan/Downloads/parse_out.txt
-done
+sed 's/exec_host/nothing/' /home/rohan/Downloads/parse_out.txt
+echo= sudo cat /home/rohan/Downloads/parse_out.txt | awk -v FS="(<exec_host>|</exec_host>)" '{print $2}'
+sed 's/exec_host/nothing/' /home/rohan/Downloads/parse_out.txt
+
+#sed -n 's/</exec_host>/nothing/' /home/rohan/Downloads/parse_out.txt
+#done
 
